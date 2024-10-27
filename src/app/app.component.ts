4cc8efb6b68f121e0480f2678414e25d64fe0543
Théo LUDWIG <contact@theoludwig.fr>
2024-10-27 16:32:23 +0100

    feat: usage of HTTPClient and ApiService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { RouterOutlet } from "@angular/router"
 import { FooterComponent } from "../components/footer/footer.component"
 import { HeaderComponent } from "../components/header/header.component"
+import { Observable } from "rxjs"
+import { Product } from "../models/product"
+import { ApiService } from "../services/api.service"
+import { AsyncPipe } from "@angular/common"
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AsyncPipe],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = "tp03_ludwig_theo"
+  public products!: Observable<Product[]>
+
+  public constructor(private readonly apiService: ApiService) {}
+
+  public ngOnInit(): void {
+    this.products = this.apiService.getProducts()
+  }
 }
